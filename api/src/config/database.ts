f7597c8c8066ce09328be94e8fcb1e1defb9e12a
Yash Krishan Gupta <yashkrishangupta@gmail.com>
2026-07-18 20:08:31 +0530

    Update database.ts

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -1,12 +1,10 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    database: process.env.DB_NAME || 'facegate_db',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'mahima_sql',
-    ssl: false,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 pool.connect()
